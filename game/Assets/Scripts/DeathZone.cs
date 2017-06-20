cc85f3434dfb869e9ae18fa37226587c9ea3ea22
Veronika <its.hard.to.do.original.email@gmail.com>
2017-06-21 00:50:07 +0300

    jump,deathzone,design,camera follows peter

---
+++ game/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour{
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		HeroPeter peter = collider.GetComponent<HeroPeter>();
+
+		if (peter != null)
+		{
+			LevelController.current.onPeterDeath(peter);
+		}
+	}
+}
