8dafe9fe2b97d5eaf42410c5bb91982df8cbc806
Veronika <its.hard.to.do.original.email@gmail.com>
2017-06-21 13:27:45 +0300

    ///

---
+++ Assets/MegScript.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MegScript : MonoBehaviour {
+
+
+	public enum Mode {
+		burn,
+		survive,
+		die
+	}
+	//starting from point .. 
+	Mode mode = Mode.burn;
+
+	Rigidbody2D myBody = null;
+
+	//initialization
+	void Start () {
+		myBody = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		//initialization 
+		Animator animator = GetComponent<Animator>();
+
+		if(mode==Mode.survive){animator.SetTrigger("live");}
+		if(mode==Mode.die)animator.SetTrigger("pray");
+	}
+
+	/*void FixedUpdate(){*/
+/*		float value = this.direction();
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		//change sides
+		if (value<0) sr.flipX = false;
+		else if (value>0) sr.flipX = true;
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+		}
+		if (mode == Mode.attack && (Time.time - last_carrot > 2.0f)) {
+			if(soundManager.Instance.isSoundOn())
+				attackAudioSource.Play();
+			Animator animator = GetComponent<Animator> ();
+			animator.SetBool ("attack", true);
+			if(sr.flipX)launchCarrot(1.0f);
+			else launchCarrot(-1.0f);
+			last_carrot=Time.time;
+		} else {
+			Animator animator = GetComponent<Animator> ();
+			animator.SetBool ("attack", false);
+		}
+
+	}
+
+	float direction(){
+		Vector3 my_pos = this.transform.position;
+		Vector3 rabbit_pos = HeroRabbit.lastRabbit.transform.position;
+		//learn situations in which orc start attack 
+		if(rabbit_pos.x>startPoint&& rabbit_pos.x<finishPoint && mode!=Mode.attack && Mathf.Abs(rabbit_pos.y-my_pos.y)<GetComponent<BoxCollider2D>().size.y) mode=Mode.attack;
+		else if((rabbit_pos.x<startPoint|| rabbit_pos.x>finishPoint) && mode==Mode.attack) mode = Mode.walkToA;
+		if(mode==Mode.walkToA){
+			if(my_pos.x > startPoint)return -1;
+			else {
+				mode = Mode.walkToB;
+				return 1;
+			}
+		}
+		if(mode==Mode.walkToB){	
+			if(my_pos.x < finishPoint)return 1;
+			else {
+				mode = Mode.walkToA;
+				return -1;
+			}
+		}
+
+		if(mode==Mode.attack){
+			SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+			if (rabbit_pos.x<my_pos.x) sr.flipX = false;
+			else if (rabbit_pos.x>my_pos.x) sr.flipX = true;
+		}
+		return 0;
+	}
+	//learn situations in which rabbit die or orc die 
+	void OnCollisionEnter2D(Collision2D col){
+
+		Collider2D collider = col.collider;
+
+		if(col.transform.tag == "Player")
+		{
+			Vector3 contactPoint = col.contacts[0].point;
+			float up = this.GetComponent<BoxCollider2D>().bounds.max.y;
+			if (Mathf.Abs (contactPoint.y - up) < 0.02f) {
+				mode = Mode.attacked;
+				StartCoroutine(dieLater ());
+			}
+			else mode=Mode.attack;
+		}
+	}
+	public void DestroyOrc(){
+		Destroy(this.gameObject);
+	}
+	void OnCollisionExit2D(Collision2D col){
+		if(col.transform.tag == "Player")
+		{
+			mode=Mode.walkToA;
+		}
+	}
+	//pause before dying and destroying 
+	IEnumerator dieLater(){
+		yield return new WaitForSeconds (0.8f);	
+		DestroyOrc ();
+	}
+
+	void launchCarrot(float direction){
+		GameObject obj = GameObject.Instantiate(this.prefabCarrot);
+
+		Vector3 my_pos = this.transform.position;
+		my_pos.y=this.GetComponent<BoxCollider2D>().bounds.center.y;
+
+		carrot carrot = obj.GetComponent<carrot>();
+		carrot.transform.position = my_pos;
+		carrot.launch(direction);
+	}*/
+
+}
---
+++ Assets/Peter/peter_scripts/HeroPeter.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroPeter : MonoBehaviour {
+	//РУХ 
+	public float speed = 1;
+	Rigidbody2D myBody = null;
+	//СТРИБОК
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
+
+
+	void Start () {
+		myBody = this.GetComponent<Rigidbody2D>();
+		//КОНТРОЛЬ ПОЗИЦІЇ
+		LevelController.current.setStartPosition (transform.position);
+	}
+
+
+	void FixedUpdate()
+	{
+		//ЧИТАННЯ КЛАВІШ - СТРІЛКИ
+		float value = Input.GetAxis("Horizontal");  
+		if (Mathf.Abs(value) > 0)
+		{
+			//РУХ
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+		}
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		//ЗМІНА НАПРЯМКУ
+		if (value < 0)
+		{
+			sr.flipX = true;
+		}
+		else if (value > 0)
+		{
+			sr.flipX = false;
+		}
+		//АНІМАЦІЯ БІГУ
+		Animator animator = GetComponent<Animator>(); // run-idle
+		if (Mathf.Abs(value) > 0)
+		{
+			animator.SetBool("run", true);
+		}
+		else
+		{
+			animator.SetBool("run", false);
+
+		}
+
+		//ВИЗНАЧЕННЯ ВІДСТАНІ , ЩО ВИМІРЮЄ , КОЛИ ПОЧИНАЄТЬСЯ СТРИБОК
+		Vector3 from = transform.position + Vector3.up * 2f;
+		Vector3 to = transform.position + Vector3.down * 1.2f;
+		int layer_id = 1 << LayerMask.NameToLayer("Ground");
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if (hit)
+		{
+			isGrounded = true;
+		}
+		else
+		{
+			isGrounded = false;
+		}
+		//ВИЗНАЧЕНЯ УМОВ ВКЛЮЧЕННЯ АНІМАЦІЇ СТРИБКУ
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			this.JumpActive = true;
+		}
+		if (this.JumpActive)
+		{
+			if (Input.GetButton("Jump"))
+			{
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime)
+				{
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			}
+			else
+			{
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+
+		if (this.isGrounded)
+		{
+			animator.SetBool("jump", false);
+		}
+		else
+		{
+			animator.SetBool("jump", true);
+		}
+		//ВИЗНАЧЕННЯ КИДАННЯ ВОГНЕГАСНИКА
+		if (Input.GetKeyDown(KeyCode.E)){
+			//LevelController.current.onPeterDeath(this);
+
+		}
+	
+	}
+	void launchExtinguisher(float direction){
+		GameObject obj = GameObject.Instantiate(this.prefabExtinguisher);
+		Vector3 my_pos = this.transform.position;
+		my_pos.y=this.GetComponent<BoxCollider2D>().bounds.center.y;
+		Extinguisher extinguisher = obj.GetComponent<extinguisher>();
+		extinguisher.transform.position = my_pos;
+		extinguisher.launch(direction);
+	}
+}
